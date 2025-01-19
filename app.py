8909c39287c88b8eb00301794cc1f598fc1fbf2c
Subha Mandol <subho.mandol00@gmail.com>
2025-01-19 19:40:44 +0530

    CSS updated

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask, request, render_template
 import requests
-
 app = Flask(__name__)
-
 # Endpoint of the model running in Google Colab (ngrok URL)
-colab_model_url = "https://b2e5-34-169-48-151.ngrok-free.app/generate"  # Replace with your actual Ngrok URL
-
+colab_model_url = "https://6687-35-227-171-114.ngrok-free.app/generate"  # Replace with your actual Ngrok URL
 def get_model_response(user_msg):
     data = {"prompt": user_msg}
     response = requests.post(colab_model_url, json=data)
@@ -13,17 +10,14 @@ def get_model_response(user_msg):
         return response.json().get('response', 'No response key in JSON')
     else:
         return "Error communicating with the model."
-
 @app.route('/')
 def input():
     return render_template('input.html')
-
 @app.route('/subjective_output', methods=['POST'])
 def subjective_output():
     subjective_question = request.form['subjective_question']
     response = get_model_response("1"+subjective_question)
     return render_template('output_subjective.html', subjective_question=subjective_question, response=response)
-
 @app.route('/objective_output', methods=['POST'])
 def objective_output():
     objective_question = request.form['objective_question']
@@ -33,6 +27,5 @@ def objective_output():
     choice_d = request.form['choice_d']
     response = get_model_response("2"+objective_question +"||||"+ choice_a +"||||" +choice_b +"||||"+choice_c +"||||"+choice_d +"||||")
     return render_template('output_objective.html', objective_question=objective_question, choice_a=choice_a, choice_b=choice_b, choice_c=choice_c, choice_d=choice_d, response=response)
-
 if __name__ == '__main__':
     app.run(debug=True)
